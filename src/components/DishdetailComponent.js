488ad5019c6af6cad826f82c192b7c8590488d43
bipin <bipinchaudhary350@gmail.com>
2020-07-16 11:49:59 +0530

    functionalComponents

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,4 +1,75 @@
-import React, { Component } from "react";
+import React from "react";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
+
+function RenderDish({ dish }) {
+  if (dish == null) {
+    return <div></div>;
+  } else {
+    return (
+      <Card>
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    );
+  }
+}
+
+function RenderComment({ comments }) {
+  if (comments == null) {
+    return <div></div>;
+  } else {
+    const com = comments.map((item) => {
+      return (
+        <li>
+          <p>{item.comment}</p>
+          <p>
+            --{item.author},&nbsp;
+            {new Intl.DateTimeFormat("en-US", {
+              year: "numeric",
+              month: "short",
+              day: "2-digit",
+            }).format(new Date(Date.parse(item.date)))}
+          </p>
+        </li>
+      );
+    });
+    return <ul className="list-unstyled">{com}</ul>;
+  }
+}
+
+const DishDetail = (props) => {
+  if (props.dish == null) {
+    return <div></div>;
+  }
+
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col-12 col-md-5 m-1">
+          <RenderDish dish={props.dish} />
+        </div>
+        <div className="col-12 col-md-5 m-1">
+          <h4>Comments</h4>
+          <RenderComment comments={props.dish.comments} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DishDetail;
+
+/*import React, { Component } from "react";
 import {
   Card,
   CardImg,
@@ -73,3 +144,4 @@ class DishDetail extends Component {
 }
 
 export default DishDetail;
+*/
